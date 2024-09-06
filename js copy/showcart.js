c860aadd012f18f4405cca33d1a504b93924f06a
nzieun <21T1020181@husc.edu.vn>
2024-09-06 18:36:05 +0700

    init project

---
+++ js copy/showcart.js
@@ -0,0 +1,8 @@
+function showcart(){
+    var x= document.getElementById("showcart");
+    if(x.style.display ==  "block"){
+        x.style.display = "none";
+    }else{
+        x.style.display = "block";
+    }
+}
